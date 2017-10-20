53ced74963e73baae7220516d5e8e6a59593577c
Marco Antonio Vollet Marson <12066726@uepg.br>
2017-10-20 18:01:10 -0200

    trying interruptions

--- Portao2.py
+++ Portao2.py
@@ -12,13 +12,25 @@ from Variables import *
 from threading import Thread
 import locale
 locale.setlocale(locale.LC_ALL, 'Portuguese')
+
+
 class mudaPorta(Thread):
+    counter_IR = 0
+    counter_RFID= 0
+    counter_CFC = 0
     def __init__(self):
         Thread.__init__(self)
+    def infraRedPortao():
+        if(counter_RFID == 1):
+            print ("Infravermelho detectado após RFID")
+            counter_IR= 1
+        else:
+            print ("Infravermelho detectado pela chave ou pelo interfone")
+
     def run(self):
-        counter_IR = 0
-        counter_RFID= 0
-        counter_CFC = 0
+        counter_RFID=0
+        counter_IR= 0
+        counter_CFC= 0
         counter_while=True
         while(counter_while):
             try:
@@ -41,7 +53,7 @@ class mudaPorta(Thread):
                         elif(uid == rfid2):
                             print("Acesso permitido - Rogério ( Ap30), RFID com UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
                         print ("Sistema Porta")
-
+                        counter_RFID=1
                         GPIO.output(31, 1) # aciona sistema relé por 1 segundo
                         time.sleep(1)
                         GPIO.output(31,0) # desativa sistema relé por 1 segundo
@@ -51,12 +63,12 @@ class mudaPorta(Thread):
 
             except KeyboardInterrupt:
                 GPIO.cleanup()
-                   # clean up GPIO on CTRL+C exit
         try:
-            GPIO.wait_for_edge(13, GPIO.RISING)
+            #GPIO.wait_for_edge(13, GPIO.RISING)
+            GPIO.add_event_detect(13, GPIO.RISING, callback=infraRedPortao, bouncetime=300)
             print "Infravermelho detectado"
             #camera action
-            #send email/information about door opened
+            #send email/information about door  . Utilizar site online para enviar email, pois raspberry pi vai travar.
             print "Esperando sinal chave fim de curso "
             GPIO.wait_for_edge(11, GPIO.RISING)
             print("Processo finalizado")
